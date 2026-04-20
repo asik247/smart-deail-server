a0e0c8a553c607405e6825b5884f358c3eda47e9
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-20 23:25:56 +0600

    crud operation done and testing thunder client

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+// TODO Midleware code here;
+app.use(cors());
+app.use(express.json());
+// ! uri code here;
+const uri = "mongodb+srv://smartDeails:5GUb0QIfeNfUO4Gd@cluster0.fdzc9ua.mongodb.net/?appName=Cluster0";
+
+// ! db client code here;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+// ! Root Api code here;
+app.get('/', (req, res) => {
+    res.send('This is Smart Detail server aaaa')
+})
+// ! run funk code here;
+async function run() {
+    try {
+        //? connect db;
+        await client.connect()
+        //! create mydb and productsColl;
+        const mydb = client.db('smartDeails');
+        const productsColl = mydb.collection('products')
+        //TODO:Get method all data using find;
+        app.get('/products',async (req,res)=>{
+            const projectFields = {title:1,condition:1}
+            const cursor = productsColl.find().project(projectFields);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        //TODO:Get method but specifiq id use and fetch data usign findOne;
+        app.get('/products/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:new ObjectId(id)};
+            const result = await productsColl.findOne(query)
+            res.send(result)
+        })
+        //TODO: Post method usign insertOne;
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsColl.insertOne(newProduct);
+            res.send(result);
+        })
+        //TODO:Update method using patch;
+        app.patch('/products/:id',async (req,res)=>{
+            const allProducts = req.body;
+            const id = req.params.id;
+            const query = {_id:new ObjectId(id)};
+            const update = {
+                $set:{
+                    name:allProducts.name,
+                    price:allProducts.price
+                }
+            }
+            const result = await productsColl.updateOne(query,update);
+            res.send(result)
+        })
+        //TODO: Delete method using delete;
+        app.delete('/products/:id',async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id:new ObjectId(id)};
+            const result = await productsColl.deleteOne(query);
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+
+
+        // ? Ping message code;
+        await client.db("admin").command({ ping: 1 })
+        console.log("Pinged your deployment.u successfully connected to MongoDB!");
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+//?Listing code here;
+app.listen(port, () => {
+    console.log(`This server runing in port: ${port}`);
+})
+/**
+ * 5GUb0QIfeNfUO4Gd
+ * smartDeails
+ */
